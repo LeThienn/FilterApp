5852740a58c1a5ef2376ad6681df68f8543a5467
LeThienIT <55796074+LeThienn@users.noreply.github.com>
2022-03-03 17:52:12 +0700

    applying image filter

--- app/src/main/java/com/example/filterapp/activities/editimage/EditImageActivity.kt
+++ app/src/main/java/com/example/filterapp/activities/editimage/EditImageActivity.kt
@@ -1,21 +1,33 @@
 package com.example.filterapp.activities.editimage
 
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.example.filterapp.activities.main.MainActivity
+import com.example.filterapp.adapters.ImageFiltersAdapter
+import com.example.filterapp.data.ImageFilter
 import com.example.filterapp.databinding.ActivityEditImageBinding
+import com.example.filterapp.listeners.ImageFilterListener
 import com.example.filterapp.utilities.displayToast
 import com.example.filterapp.utilities.show
 import com.example.filterapp.viewmodel.EditImageViewModel
+import jp.co.cyberagent.android.gpuimage.GPUImage
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class EditImageActivity : AppCompatActivity() {
+class EditImageActivity : AppCompatActivity(), ImageFilterListener {
 
     private lateinit var binding: ActivityEditImageBinding
 
     private val viewModel: EditImageViewModel by viewModel()
+
+    private lateinit var gpuImage: GPUImage
+
+    // Image bitmaps
+    private lateinit var originalBitmap: Bitmap
+    private val filterBitmap = MutableLiveData<Bitmap>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +45,43 @@ class EditImageActivity : AppCompatActivity() {
             binding.previewProgressBar.visibility =
                 if (dataState.isLoading) View.VISIBLE else View.GONE
             dataState.bitmap?.let { bitmap ->
-                binding.imagePreview.setImageBitmap(bitmap)
-                binding.imagePreview.show()
+                // for the first time "filtered image = original image"
+                originalBitmap = bitmap
+                filterBitmap.value = bitmap
+                with(originalBitmap){
+                    gpuImage.setImage(this)
+                    binding.imagePreview.show()
+                    viewModel.loadImageFilters(bitmap)
+                }
             } ?: kotlin.run {
                 dataState.error?.let { error ->
                     displayToast(error)
                 }
             }
         })
+
+        viewModel.imageFilterUiState.observe(this, {
+            val imageFilterDataState = it ?: return@observe
+            binding.imageFiltersProgressBar.visibility =
+                if(imageFilterDataState.isLoading) View.VISIBLE else View.GONE
+            imageFilterDataState.imageFilters?.let { imageFilters ->
+                ImageFiltersAdapter(imageFilters, this).also { adapter ->
+                    binding.filtersRecyclerView.adapter = adapter
+                }
+            }?: kotlin.run {
+                imageFilterDataState.error?.let { error ->
+                    displayToast(error)
+                }
+            }
+        })
+
+        filterBitmap.observe(this, { bitmap ->
+            binding.imagePreview.setImageBitmap(bitmap)
+        })
     }
 
     private fun prepareImagePreview() {
+        gpuImage = GPUImage(applicationContext)
         intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
             viewModel.prepareImagePreview(imageUri)
         }
@@ -51,5 +89,27 @@ class EditImageActivity : AppCompatActivity() {
 
     private fun setListeners() {
         binding.imageBack.setOnClickListener { onBackPressed() }
+
+        /*
+        this will show original image when we long click the imageview util we release click
+        So that we can see difference between original image and filtered image
+         */
+        binding.imagePreview.setOnLongClickListener {
+            binding.imagePreview.setImageBitmap(originalBitmap)
+            return@setOnLongClickListener false
+        }
+
+        binding.imagePreview.setOnClickListener {
+            binding.imagePreview.setImageBitmap(filterBitmap.value)
+        }
+    }
+
+    override fun onFilterSelected(imageFilter: ImageFilter) {
+        with(imageFilter){
+            with(gpuImage){
+                setFilter(filter)
+                filterBitmap.value = bitmapWithFilterApplied
+            }
+        }
     }
 }
